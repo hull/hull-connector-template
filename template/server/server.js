4ce7c980d42af94d1ccac74b40c7318437dfb854
Michal Raczka <michal@raczka.me>
2018-06-04 16:05:35 +0200

    WIP - example

--- template/server/server.js
+++ template/server/server.js
@@ -1,14 +1,12 @@
 /* @flow */
 import type { $Application, $Response } from "express";
-import type { TRequest, THullReqContext } from "hull";
+// import type { TRequest, THullReqContext } from "hull";
 
-const {
-  smartNotifierHandler,
-  THullUserUpdateMessage
-} = require("hull/lib/utils");
+const { notificationHandler, batchHandler, schedulerHandlers } = require("hull/lib/handlers");
 const bodyParser = require("body-parser");
 
 const { webhookHandler, statusCheck, updateUser } = require("./actions");
+const notificationConfiguration = require("./notification-configuration");
 
 function server(app: $Application, { token }: Object): $Application {
   app.get("/admin.html", (req: TRequest, res: $Response) => {
@@ -17,40 +15,11 @@ function server(app: $Application, { token }: Object): $Application {
 
   app.all("/webhook", bodyParser.json(), webhookHandler);
 
-  app.all("/status", statusCheck);
+  app.use("/status", schedulerHandler(statusCheck));
 
-  app.use(
-    "/batch",
-    smartNotifierHandler({
-      userHandlerOptions: {
-        groupTraits: false
-      },
-      handlers: {
-        "user:update": updateUser
-      }
-    })
-  );
-
-  app.use(
-    "/smart-notifier",
-    smartNotifierHandler({
-      handlers: {
-        "user:update": (
-          ctx: THullReqContext,
-          messages: Array<THullUserUpdateMessage>
-        ) => {
-          if (ctx.smartNotifierResponse) {
-            ctx.smartNotifierResponse.setFlowControl({
-              type: "next",
-              in: parseInt(process.env.FLOW_CONTROL_IN, 10) || 1000,
-              size: parseInt(process.env.FLOW_CONTROL_SIZE, 10) || 100
-            });
-          }
-          return updateUser(ctx, messages);
-        }
-      }
-    })
-  );
+  app.use("/batch", batchHandler(notificationConfiguration));
+  app.use("/account-batch", batchHandler(notificationConfiguration));
+  app.use("/smart-notifier", notificationHandler(notificationConfiguration));
 
   // you can use object spread syntax
   const a = { a: 1 };
